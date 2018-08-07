358773515c864057e4e233032fdf8dd85015edca
survivorbat <mheijden15@avans.nl>
2018-08-07 09:57:26 +0200

    Remove command constructor trim and tolowercase

--- src/ts/utils/Command.ts
+++ src/ts/utils/Command.ts
@@ -2,17 +2,13 @@ import { ICommand } from '../abstract/utils/ICommand';
 import { ICommandExecutor } from '../abstract/utils/ICommandExecutor';
 
 export class Command implements ICommand {
-	private readonly useValue: string
-
 	constructor(
-		useValue: string, 
+		private readonly useValue: string,
 		private readonly useExecutor: ICommandExecutor,
-		public readonly commandAsText: string = ""
-	) { 
-		this.useValue = useValue.trim().toLowerCase()
-	}
+		public readonly commandAsText: string = ''
+	) {}
 
 	public execute() {
-		this.useExecutor.execute(this.useValue)
+		this.useExecutor.execute(this.useValue);
 	}
 }
